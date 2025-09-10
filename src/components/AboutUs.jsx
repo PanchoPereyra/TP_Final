cce84619c97d797f1e1e88bb2d66f6a1a178c26f
Pancho_Pereyra <panchopereyra@gmail.com>
2025-09-10 09:17:55 -0300

    Probando hacer cambios y que se reflejen en Netlify

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,7 +3,7 @@ function AboutUs() {
     return (
         <>
             <section className="bg-orange-50 px-6 py-16">
-                <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 items-center border">
+                <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
                     <div className="">
 
                         {/* IMAGEN*/}
