46dfeb3f715375d9d69d60f507b1d16cce1c9809
Pancho_Pereyra <panchopereyra@gmail.com>
2025-09-10 13:12:04 -0300

    Acomodé margen de h2

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -15,7 +15,7 @@ function AboutUs() {
                     </div>
 
                     <div className="bg-white pl-3 pt-8 pr-8 text-justify w-3/4 mr-30">
-                        <h2 className="text-5xl text-gray-900 font-fraunces text-justify ml-30">About Us</h2>
+                        <h2 className="text-5xl text-gray-900 font-fraunces text-justify ml-25">About Us</h2>
                         <hr className="border-gray-300 w-[25%] border-t-3 mt-2 ml-25" ></hr>
                         <p className="text-gray-600 text-justify ml-25 pt-5 pb-8 pr-20">
                             The target definition of a charity organization ahead of
